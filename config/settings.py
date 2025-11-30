37682559d394bfab8d1902e3dbee2c2cc3676eac
Henry Ndou <ndou.research@gmail.com>
2025-11-30 23:50:23 +0200

    Applied initial migrations for all custom apps

--- config/settings.py
+++ config/settings.py
@@ -4,8 +4,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(BASE_DIR / "apps"))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
